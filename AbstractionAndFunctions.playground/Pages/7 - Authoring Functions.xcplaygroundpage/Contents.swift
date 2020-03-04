3f656acb98d1266ae33eb797223a1a9ec2856724
Chen Sihan <schen@lcs.on.ca>
2020-03-04 15:47:40 -0500

    Prevent false input by returning nil

--- AbstractionAndFunctions.playground/Pages/7 - Authoring Functions.xcplaygroundpage/Contents.swift
+++ AbstractionAndFunctions.playground/Pages/7 - Authoring Functions.xcplaygroundpage/Contents.swift
@@ -27,26 +27,32 @@ import Foundation
  */
 // Begin your work here...
 
+// Guard Statements
+//// Ensure that we have reasonable values
+//guard length > 0, width > 0 else {
+//    // We have one or more bad input
+//    return nil
+//}
+
+
 
 // MARK: Perimeter
 // Triangle Circumference
-func trianglePerimeter(a: Double, b: Double, c: Double) -> String {
+func trianglePerimeter(a: Double, b: Double, c: Double) -> Double? {
     // Properties of triangles
-    if a+b>c && b+c>a && a+c>b {
-    return String(a + b + c)
-    } else {
-        
-    // Prevent False Input
-    return "Please make sure it is a triangle."
-    }
     
-    // Not to be executed but to get rid of the debugger
-    return ""
+    guard a > 0, b > 0, c > 0, a+b>c && b+c>a && a+c>b  else {
+        return nil
+    }
+ // Perimeter
+    return a+b+c
 }
-// Test case #1
+// Test case #1: a:10, b:20, c:29, result: 59
 trianglePerimeter(a: 10, b: 20, c: 29)
-// Test case #2
+// Test case #2: a:10, b:20, c:30 result: ask for another input
 trianglePerimeter(a: 10, b: 20, c: 30)
+
+trianglePerimeter(a: -2, b: 2, c: 2)
 
 
 // Circumference of Circle
@@ -54,7 +60,7 @@ func circlePerimeter(radius: Double) -> Double {
     return Double.pi * 2 * radius
 }
 
-// Test case
+// Test case: radius: 10, result: 20pi = 62.8318...
 circlePerimeter(radius: 10)
 
 
@@ -65,7 +71,7 @@ func parallelogramArea(base: Double, height: Double) -> Double {
     return base * height
 }
 
-// Test case
+// Test case: base: 5 , height: 6, result: 30
 parallelogramArea(base: 5, height: 6)
 
 
@@ -74,7 +80,7 @@ func circleArea(radius: Double) -> Double {
     return Double.pi * pow(radius, 2.0)
 }
 
-// Test Case
+// Test Case: radius: 10, result: 100pi = 314.1592...
 circleArea(radius: 10)
 
 // MARK: Surface Area
@@ -83,18 +89,18 @@ func sphereSurfaceArea(radius: Double) -> Double {
     return Double.pi * 4 * pow(radius, 2.0)
 }
 
-// Test case
+// Test case: radius 5, result: 100pi = 314.1592...
 sphereSurfaceArea(radius: 5)
 
 
 // Surface Area of Cone
 func coneSurfaceArea(radius: Double, slantHeight: Double) -> Double {
-    let baseArea = pow(radius, 2.0) + Double.pi
+    let baseArea = pow(radius, 2.0) * Double.pi
     let lateralSurface = Double.pi * radius * slantHeight
     return baseArea + lateralSurface
 }
 
-// Test case
+// Test case: radius: 5, slantHeight: 10, result: 75pi = 235.6194...
 coneSurfaceArea(radius: 5, slantHeight: 10)
 
 
@@ -104,7 +110,7 @@ func cylinderVolume(radius: Double, height: Double) -> Double {
     return Double.pi * pow(radius, 2.0) * height
 }
 
-// Test case
+// Test case: radius: 5, height: 10, result: 250pi = 785.3981...
 cylinderVolume(radius: 5, height: 10)
 
 
@@ -114,7 +120,7 @@ func rectangularPrismVolume(length: Double, width: Double, height: Double) -> Do
     return width * length * height
 }
 
-// Test case
+// Test case: length:10, width:10, height:10, result:1000
 
 rectangularPrismVolume(length: 10, width: 10, height: 10)
 /*:
